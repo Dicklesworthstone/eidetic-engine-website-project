244cbc9e5e7edd6022abc7cad82b4fbf5159b915
Dicklesworthstone <jeff141421@gmail.com>
2025-04-19 12:15:13 +0000

    Fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverMinification: false,  // Disable minification to prevent variable access issues
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
